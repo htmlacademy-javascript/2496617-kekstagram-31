b628c564833b969677448ab0c10a7f5466555659
alexx-kis <kislov@advline.ru>
2024-03-27 10:31:05 +0300

    api.js: addresses modified

--- js/api.js
+++ js/api.js
@@ -3,14 +3,17 @@ import { showSuccessMessage, showDataErrorMessage } from './success-and-error-me
 // $======================== GET AND SEND DATA ========================$ //
 // $======================== GET AND SEND DATA ========================$ //
 
-const GET_DATA_URL = 'https://31.javascript.htmlacademy.pro/kekstagram/data';
-const SEND_DATA_URL = 'https://31.javascript.htmlacademy.pro/kekstagram';
+const BASE_URL = 'https://31.javascript.htmlacademy.pro/kekstagram';
+const ROUTE = {
+  GET_DATA: '/data',
+  SEND_DATA: '/'
+};
 
 //@ функция, получающая данные с сервера
 const getData = (
   // onSuccess //? эта функция не работает, как ожидается по показанному в скринкасте
 ) => {
-  const data = fetch(GET_DATA_URL) //? если адрес неверный, то выводится ошибка в консоль, это норм?
+  const data = fetch(`${BASE_URL}${ROUTE.GET_DATA}`) //? если адрес неверный, то выводится ошибка в консоль, это норм?
     .then((response) => {
       if (!response.ok) {
         throw new Error(`${response.status} - ${response.statusText}`);
@@ -32,7 +35,7 @@ const pictures = await getData();
 
 //@ функция, отправляющая данные на сервер
 const sendData = (onSuccess, onFail, body) => {
-  fetch(SEND_DATA_URL,
+  fetch(`${BASE_URL}${ROUTE.GET_DATA}`, //? если адрес неверный, то выводится ошибка в консоль, это норм?
     {
       method: 'POST',
       body: body,
