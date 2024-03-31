302ad7689a85242e039a075e6176179be8c989c1
Alexander <fenaethylaamiinloever@gmail.com>
2024-03-31 10:37:28 +0300

    + pictures filtration

--- js/main.js
+++ js/main.js
@@ -3,3 +3,4 @@ import './render-thumbnails.js';
 import './open-modal.js';
 import './upload-form.js';
 import './upload-modal.js';
+import './filter.js';
--- js/util.js
+++ js/util.js
@@ -37,5 +37,38 @@ const checkInputsFocus = () => {
   }
 };
 
+//@ функция, "устраняющая дребезг"
+const debounce = (callback, timeoutDelay) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+//@ функция, возвращающая перемешанный массив на основе переданного
+/// amount - количество отобранных элементов
+const shuffleArray = (array, amount) => array.slice().sort(() => Math.random() - 0.5).slice(0, amount);
+
+//@ функция, возвращающая массив картинок, отсортированный по убыванию количества комментариев
+const sortPicturesByCommentsAmount = (array) => array.slice().sort((a, b) => b.comments.length - a.comments.length);
+
+//@ функция, очищающая контейнер
+const clearContainer = (container, elementClass) => {
+  const elements = container.querySelectorAll(`.${elementClass}`);
+  elements.forEach((element) => {
+    element.remove();
+  });
+};
+
+//@ функция, переключающая активные элементы (фильтры)
+const switchActiveItem = (container, selectedItem, activeClass) => {
+  const active = container.querySelector(`.${activeClass}`);
+  if (active) {
+    active.classList.remove(activeClass);
+  }
+  selectedItem.classList.add(activeClass);
+};
+
 // &------------------------ EXPORT ------------------------& //
-export { getRandomPositiveInteger, getRandomElement, generateId, isEscKey, splitInput, checkInputsFocus };
+export { getRandomPositiveInteger, getRandomElement, generateId, isEscKey, splitInput, checkInputsFocus, debounce, shuffleArray, clearContainer, switchActiveItem, sortPicturesByCommentsAmount };
